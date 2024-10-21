4823a5be21815fbab859191e3ebd64f441f862cd
Andrii Babak <andreybabak1@icloud.com>
2024-10-21 10:09:35 +0100

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Andrey Babak on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
